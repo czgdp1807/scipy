f5caaa000caf296f537be2bf2c2434d0f6f17a93
Gagandeep Singh <gdp.1807@gmail.com>
2024-06-12 20:04:21 +0530

    CI: Avoid logging

--- tools/install_intel_oneAPI_windows.bat
+++ tools/install_intel_oneAPI_windows.bat
@@ -8,7 +8,7 @@ set COMPONENTS=%2
 
 
 curl.exe --output %TEMP%\webimage.exe --url %URL% --retry 5 --retry-delay 5
-start /b /wait %TEMP%\webimage.exe -s -x -f webimage_extracted --log extract.log
+start /b /wait %TEMP%\webimage.exe -s -x -f webimage_extracted
 del %TEMP%\webimage.exe
 if "%COMPONENTS%"=="" (
   webimage_extracted\bootstrapper.exe -s --action install --eula=accept -p=NEED_VS2017_INTEGRATION=0 -p=NEED_VS2019_INTEGRATION=0 -p=NEED_VS2022_INTEGRATION=0
